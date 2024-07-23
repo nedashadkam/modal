efc829ee107e6ef26618528b8b5193fa74d2f4b8
neda <neda.shadkam2003@gmail.com>
2024-07-23 15:30:41 +0330

    first commit

---
+++ modal.js
@@ -0,0 +1,12 @@
+function toClose() {
+
+    let y = document.getElementById("container").style;
+
+    y.display = "none";
+
+}
+
+function output() {
+    document.getElementById("container").style.display = 'block';
+    document.getElementById('container').style.position = 'absolute'
+}
